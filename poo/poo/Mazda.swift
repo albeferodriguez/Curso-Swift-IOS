ae8c02aeab15d5500120094db8c4b9b5902ed4ad
Alberto Fernández-Baíllo <albeferodriguez@gmail.com>
2019-12-20 23:22:43 +0100

    Created some buttons linked to the view

---
+++ poo/poo/Mazda.swift
@@ -0,0 +1,27 @@
+//
+//  Mazda.swift
+//  poo
+//
+//  Created by Alberto on 20/12/2019.
+//  Copyright © 2019 Alberto. All rights reserved.
+//
+
+import Foundation
+
+
+class Mazda {
+    
+    var size = "Medium"
+    var numDoors = 4
+    var color = "Blue"
+    var prize = "200000 USD"
+    
+    func turnOn(){
+        print("The car turned on")
+    }
+    
+    func speedUp(){
+        print("The car speeded up")
+    }
+    
+}
--- poo/poo/ViewController.swift
+++ poo/poo/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelTitle: UILabel!
+    
+    var mazdaObject: Mazda?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        labelTitle.text = "Fábrica de Automóviles"
     }
 
-
+    @IBAction func createMazdaObject(_ sender: UIButton) {
+        mazdaObject = Mazda()
+    }
+    
+    @IBAction func showMazdaProperties(_ sender: UIButton) {
+        print(
+            "The size of the mazda is \(mazdaObject!.size), it has \(mazdaObject!.numDoors) and it is \(mazdaObject!.color) and it coast \(mazdaObject!.prize)"
+        )
+    }
+    
+    @IBAction func turnOnMazda(_ sender: UIButton) {
+        mazdaObject!.turnOn()
+    }
+    @IBAction func speedUpMazda(_ sender: UIButton) {
+        mazdaObject!.speedUp()
+    }
 }
 
